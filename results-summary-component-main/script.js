e49c392fb0d1f86130904c7d2f28cd5fe84a0453
fnurrahmah125 <fnurrahmah125@gmail.com>
2024-02-06 17:52:04 +0700

    add new project

---
+++ results-summary-component-main/script.js
@@ -0,0 +1,19 @@
+const summary = document.querySelector(".summary-wrapper");
+
+fetch("./data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((data) => {
+      summary.insertAdjacentHTML(
+        "beforeend",
+        `<div class="summary__score">
+            <div class="summary__score-left">
+                <img src="${data.icon}" />
+                <span>${data.category}</span>
+            </div>
+            <div class="summary__score-right">${data.score}<span>/ 100</span></div>
+        </div>`
+      );
+    });
+  })
+  .catch((err) => console.log(err));
